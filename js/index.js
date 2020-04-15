076c1ae0785c5184e5647d3792c6fdaed52215c9
RihardXXX <yusupa.akaeva@yandex.ru>
2020-04-16 01:26:37 +0300

    over portfolio layot

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+// $(document).ready(function () {
+
+//     $('.button[filter="web"]').click(function () {
+//         // $('.filter > div').hide(300);
+//         console.log('test');
+//     })
+// });
+
+$(document).ready(function () {
+    $('button[filter="web"]').click(function () {
+        $('.filter > div').hide(400);
+    });
+});
